997d1958cba366ffb847077196db086f4bc30442
Bininin <89152960+Bininin@users.noreply.github.com>
2022-05-22 18:31:18 +0300

    Add files via upload

--- spiders/instagram.py
+++ spiders/instagram.py
@@ -11,11 +11,10 @@ class InstaSpider(scrapy.Spider):
     allowed_domains = ['instagram.com']
     start_urls = ['https://www.instagram.com/']
     inst_login_link = 'https://www.instagram.com/accounts/login/ajax/'
-    inst_login = 'Onliskill_udm'
-    inst_pwd = '#PWD_INSTAGRAM_BROWSER:10:1650388687:AUVQAPLsNiCtdG0b660bL/la/fAfzNJ0AaVNGPhAI7fwS9ANR85sT7Kjag60UVTeviSs34AXFch4cAYMc8Pq56W6i7ntwpu2ucSOa3aIY3LRVrPRqB2XvkxeB+KW6C2TQEPNVbnxpAqk8m4yOJg='
-    parse_user = 'techskills_2022'
-    inst_graphql_link = 'https://www.instagram.com/graphql/query/?'
-    posts_hash = '396983faee97f4b49ccbe105b4daf7a0'
+    inst_login = 'mamanata1113'
+    inst_pwd = '#PWD_INSTAGRAM_BROWSER:10:1650613524:Ac1QAAxZwkH4ALEcKc+FFWZDzJacsMkGewGtQqOxn+fYTv5nSz7uc85UP2Ph9952id0jot6e+QCtyxCkaCX4j/Dyzln5LdOyP2XiKkj/06r3qfXeLL/pCBpnyYHRWUT8KWLE5/1cqgCH45+B4g=='
+    parse_users = ['zzagarakiss','calmerlone']
+    inst_friendships_link = 'https://i.instagram.com/api/v1/friendships'
 
 
     def parse(self, response: HtmlResponse):
@@ -31,48 +30,74 @@ class InstaSpider(scrapy.Spider):
     def login(self, response: HtmlResponse):
         j_body = response.json()
         if j_body.get('authenticated'):
-
-            yield response.follow(
+            for user in self.parse_users:
+                yield response.follow(
                 f'/{self.parse_user}',
                 callback=self.user_data_parse,
                 cb_kwargs={'username': self.parse_user}
             )
     def user_data_parse(self, response: HtmlResponse, username):
         user_id = self.fetch_user_id(response.text, username)
-        variables = {'id': user_id,
-                     'first': 12}
-        url_posts = f'{self.inst_graphql_link}query_hash={self.posts_hash}&{urlencode(variables)}'
-        yield response.follow(url_posts,
-                              callback=self.user_posts_parse,
+        variables = {'count': 12}
+        url_followers = f'{self.inst_friendships_link}/{user_id}/followers/?{urlencode(variables)}&search_surface=follow_list_page'
+        yield response.follow(url_followers,
+                              callback=self.user_followers_parse,
                               cb_kwargs={'username': username,
                                          'user_id': user_id,
-                                         'variables': deepcopy(variables)})
+                                         'variables': deepcopy(variables)},
+                              headers={'User-Agent': 'Instagram 155.0.0.37.107'})
+
+        url_followings = f'{self.inst_friendships_link}/{user_id}/followings/?{urlencode(variables)}&search_surface=follow_list_page'
+        yield response.follow(url_followings,
+                              callback=self.user_followings_parse,
+                              cb_kwargs={'username': username,
+                                         'user_id': user_id,
+                                         'variables': deepcopy(variables)},
+                              headers={'User-Agent': 'Instagram 155.0.0.37.107'})
 
 
-    def user_posts_parse(self, response: HtmlResponse, username, user_id, variables):
+
+    def user_followers_parse(self, response: HtmlResponse, username, user_id, variables):
         j_data = response.json()
-        page_info = j_data.get('data').get('user').get('edge_owner_to_timeline_media').get('page_info')
-        if page_info.get('has_next_page'):
-            variables['after'] = page_info.get('end_cursor')
-            url_posts = f'{self.inst_graphql_link}query_hash={self.posts_hash}&{urlencode(variables)}'
+        if j_data.get('next_max_id'):
+            variables['max_id'] = j_data.get('next_max_id')
+            url_posts = f'{self.inst_friendships_link}/{user_id}/followers/?{urlencode(variables)}&search_surface=follow_list_page'
             yield response.follow(url_posts,
-                                  callback=self.user_posts_parse,
+                                  callback=self.user_followers_parse,
                                   cb_kwargs={'username': username,
                                              'user_id': user_id,
                                              'variables': deepcopy(variables)},
                                   headers={'User-Agent': 'Instagram 155.0.0.37.107'})
-        posts = j_data.get('data').get('user').get('edge_owner_to_timeline_media').get('edges')
-        for post in posts:
+        followers = j_data.get('users')
+        for follower in followers:
             item = InstaparserItem(
-                user_id=user_id,
-                username=username,
-                photo=post.get('node').get('display_url'),
-                likes=post.get('node').get('edge_media_preview_like').get('count'),
-                post_data=post.get('node')
-            )
+                user_id=follower.get('pk'),
+                username=follower.get('username'),
+                fullname=follower.get('full_name'),
+                photo=follower.get('profile_pic_url'),
+                all_info=follower.get('Object'))
             yield item
 
-
+    def user_followings_parse(self, response: HtmlResponse, username, user_id, variables):
+        j_data = response.json()
+        if j_data.get('next_max_id'):
+            variables['max_id'] = j_data.get('next_max_id')
+            url_followings = f'{self.inst_friendships_link}/{user_id}/followings/?{urlencode(variables)}&search_surface=follow_list_page'
+            yield response.follow(url_followings,
+                                  callback=self.user_followings_parse,
+                                  cb_kwargs={'username': username,
+                                             'user_id': user_id,
+                                             'variables': deepcopy(variables)},
+                                  headers={'User-Agent': 'Instagram 155.0.0.37.107'})
+        followings = j_data.get('users')
+        for following in followings:
+            item = InstaparserItem(
+                user_id=following.get('pk'),
+                username=following.get('username'),
+                fullname=following.get('full_name'),
+                photo=following.get('profile_pic_url'),
+                all_info=following.get('Object'))
+            yield item
 
 
 
